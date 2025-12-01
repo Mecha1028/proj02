0fa8b3d69d465c4d8cdbf25096aa9b6dba42c13b
Youbing Zhao <zhaoyoubing@gmail.com>
2025-12-01 11:14:03 +0000

    adding comments in initRenderToTexture()

--- src/main.cpp
+++ src/main.cpp
@@ -49,11 +49,12 @@ std::shared_ptr<Mesh> teapot;
 
 void initRenderToTexture()
 {
-    // Generate and bind the framebuffer
+    // ------------------------------------------------------------------
+    // Generate and bind the framebuffer for Step 1 scene offscreen render
     glGenFramebuffers(1, &colourFBO);
     glBindFramebuffer(GL_FRAMEBUFFER, colourFBO);
 
-    // Create the texture object
+    // Create the texture object for storing the scene render
     glGenTextures(1, &colourTex);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, colourTex);
@@ -66,22 +67,23 @@ void initRenderToTexture()
     GLenum drawBuffers[] = {GL_COLOR_ATTACHMENT0};
     glDrawBuffers(1, drawBuffers);
 
-    // Create the depth buffer
+    // Create the depth buffer to the framebuffer
     GLuint depthBuf;
     glGenRenderbuffers(1, &depthBuf);
     glBindRenderbuffer(GL_RENDERBUFFER, depthBuf);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
 
-    // Bind the depth buffer to the FBO
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
-                              GL_RENDERBUFFER, depthBuf);
+    // Attach the depth buffer to colourFBO
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuf);
 
-    // Create an FBO for the bright-pass filter and blur
+    // ------------------------------------------------------------------
+    // Create an FBO for Step 2,3,4 the bright-pass filter and blur
     glGenFramebuffers(1, &blurFBO);
     glBindFramebuffer(GL_FRAMEBUFFER, blurFBO);
 
     // the bloom buffer should use a lower resolution to better support blurring 
     // such as width/4 and height/4 or more
+    // Downsampling is equivalent to box blur
     bloomBufWidth = width / 4;
     bloomBufHeight = height / 4;
 
@@ -89,15 +91,15 @@ void initRenderToTexture()
     glGenTextures(2, blurtex);
     glActiveTexture(GL_TEXTURE2);
 
-    // blur texture 1
+    // set parameters for blur texture 1
     glBindTexture(GL_TEXTURE_2D, blurtex[0]);
     glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB32F, bloomBufWidth, bloomBufHeight);
 
-    // blur texture 2
+    // set parameters for blur texture 2
     glBindTexture(GL_TEXTURE_2D, blurtex[1]);
     glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB32F, bloomBufWidth, bloomBufHeight);
 
-    // Bind texture 1 to the FBO
+    // Bind texture 1 to blurFBO
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, blurtex[0], 0);
 
     // Unbind the framebuffer, and revert to default framebuffer
